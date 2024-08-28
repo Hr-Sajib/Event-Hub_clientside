e738146faad537368c62f3ce43e92e3383463c4a
hrSajib <harun15-4293@diu.edu.bd>
2024-08-28 18:37:28 +0600

    video added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,14 +14,46 @@ const Home = () => {
 
             {/* Events div */}
             <div className='border-[5px] border-[#f79f47] w-[65%] mr-5 rounded-3xl p-4'>
+                
                 {/* Row one */}
-                <div>
-                    {/* Event type 1 */}
+                <div className="flex gap-3">
+                    {/* Event type Seminar */}
                     <div id="event1Container" className='relative rounded-2xl w-[500px] h-[360px] group overflow-hidden'>
                         <img className='w-[500px] h-[360px] rounded-2xl object-cover object-right transition-transform duration-500 ease-in-out group-hover:scale-110' src="https://i.ibb.co/bWHRfhc/1000-F-670307649-4br-Foy-XTCw9-Uc-P3o-NDJRjbqu48-XTHe4-L.jpg" alt="" />
-                        <div id="boxSeminar" className='w-[450px] h-[330px] border-2 border-white rounded-2xl border-r-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
-                        <div id="labelSeminar" className='text-orange-500 w-[500px] bg-white text-2xl absolute bottom-8 left-0 transition-all duration-300 ease-in-out group-hover:left-20'>
+                        <div id="boxSeminar" className='w-[450px] h-[330px] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-r-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
+                        <div id="labelSeminar" className='text-orange-500 border-t-[5px] border-orange-400 w-[500px] bg-white text-2xl absolute bottom-8 left-0 transition-all duration-300 ease-in-out group-hover:left-20'>
                             <button className='font-josefin-sans relative left-0 transition-all duration-300 ease-in-out group-hover:left-10'>SEMINARS</button>
+                        </div>
+                    </div>
+
+                   {/* Event type Party */}
+                    <div id="event1Container" className='relative rounded-2xl w-[610px] h-[360px] group overflow-hidden'>
+                        
+                    <iframe 
+                        className='h-[390px] w-[680px] transition-transform duration-500 ease-in-out group-hover:scale-110' 
+                        src="https://www.youtube.com/embed/n_cdSzb2z58?autoplay=1&controls=0&modestbranding=1&loop=1&playlist=n_cdSzb2z58" 
+                        frameBorder="0" 
+                        allow="autoplay; encrypted-media" 
+                        allowFullScreen
+                        title="YouTube Video"
+                    ></iframe>                        
+                        <div id="boxSeminar" className='w-[570px] h-[330px] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-r-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
+                        <div id="labelSeminar" className='text-orange-500 border-b-[5px] border-orange-400 w-[680px] bg-white text-2xl absolute bottom-8 left-0 transition-all duration-300 ease-in-out group-hover:left-20'>
+                            <button className='font-josefin-sans relative left-0 transition-all duration-300 ease-in-out group-hover:left-10'>PARTIES & CONCERTS</button>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                {/* Row two  */}
+                <div>
+                    {/* Event type Wedding */}
+                    <div id="event2Container" className='mt-3 relative rounded-2xl w-[600px] h-[380px] group overflow-hidden'>
+                        <img className='w-[600px] h-[460px] rounded-2xl object-cover object-right transition-transform duration-500 ease-in-out group-hover:scale-110' src="https://i.ibb.co/XFG11WC/wedding.jpg" alt="" />
+                        <div id="boxSeminar" className='w-[550px] h-[350px] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-l-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
+                        <div id="labelSeminar" className='text-orange-500 border-b-[5px] border-orange-400 w-[600px] bg-white  text-2xl absolute bottom-10 left-0 transition-all duration-300 ease-in-out group-hover:left-20'>
+                            <button className='font-josefin-sans relative left-0 transition-all duration-300 ease-in-out group-hover:left-10'>WEDDINGS</button>
                         </div>
                     </div>
                 </div>
@@ -30,4 +62,6 @@ const Home = () => {
     );
 };
 
+
+// https://youtu.be/n_cdSzb2z58?si=r-qB9-qB9LCMZ7bS
 export default Home;
