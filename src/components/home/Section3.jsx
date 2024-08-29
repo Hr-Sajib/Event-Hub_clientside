c214fdf8c2cc45eecbe86d39a1f9c54bee0febc8
hrSajib <harun15-4293@diu.edu.bd>
2024-08-29 13:53:44 +0600

    section3

--- src/components/home/Section3.jsx
+++ src/components/home/Section3.jsx
@@ -12,10 +12,69 @@ const Section3 = () => {
 
     return (
     <>
-            <div  className="my-36 px-36 py-16 pl-[260px] bg-gradient-to-r from-orange-100 to-orange-00 flex font-josefin-sans">
-            
-            <img data-aos="fade-up" className="h-[600px] object-cover object-right w-[600px] rounded-br-[100px] border-b-2 border-r-2 border-black" src="https://i.ibb.co/qDzgfJX/happy-successful-businessman-winning-fist-pump-rejoice-shouting-yes.png" alt="" />
-            <div data-aos="fade-left" className="border-t-2 border-l-2 rounded-tl-[100px] pt-32 pl-2 relative bottom-[100px] right-[2px] border-black h-[600px] w-[700px]">
+      <div className="flex gap-4 max-w-[1500px] mx-auto mb-36">
+        <img data-aos="fade-right" className="w-[40%] rounded-3xl" src="https://i.ibb.co/KNfrjr5/12643939-5013256.jpg" alt="" />
+        {/* cards div  */}
+        <div className="font-josefin-sans">
+           
+            {/* row one  */}
+            <div className="flex gap-2">
+                {/* projects  */}
+                <div data-aos="zoom-in" className="bg-orange-900 p-10 rounded-2xl w-[500px] flex gap-3">
+                    <div className=" rounded-full h-[110px] w-[110px] flex items-center justify-center bg-white">
+                        <img className="h-[60px]" src="https://i.ibb.co/NF2gp9R/calendar-17382867.png" alt="" />
+                    </div>
+                    <div>
+                        <div className="flex justify-between">
+                            <p className="text-white text-7xl">298</p>
+                            <p className="text-6xl text-white -rotate-45 relative bottom-5 left-8">→</p>
+                        </div>
+                        <p className="text-white text-4xl">Events Completed</p>
+                    </div>
+                </div>
+                {/* awards  */}
+                <div data-aos="zoom-in" className="bg-orange-900 p-10 rounded-2xl w-[300px]">
+                    <p className="text-cyan-100 text-7xl">11</p>
+                    <p className="text-white text-4xl">Awards Won</p>
+                </div>
+            </div>
+
+
+            {/* row two  */}
+            <div className="flex gap-2 mt-2">
+                 {/* Experience  */}
+                 <div data-aos="zoom-in" className="bg-orange-900 px-3 pt-3 rounded-2xl w-[300px] flex justify-center items-center gap-3">
+                    <p className="text-white text-[100px]">12</p>
+                    <p className="text-orange-100 text-4xl">Years Of Experience</p>
+                </div>
+                {/* Countries  */}
+                <div data-aos="zoom-in" className="bg-orange-900 p-2 pl-3 justify-center items-center rounded-2xl  flex gap-3">
+                    <div className=" rounded-full h-[110px] w-[110px] flex items-center justify-center bg-white">
+                        <img className="h-[60px]" src="https://i.ibb.co/hDtht4X/globe-16267960.png" alt="" />
+                    </div>
+                    <div>
+                        <div className="flex justify-between">
+                            <p className="text-yellow-200 text-7xl ">7</p>
+                            {/* <p className="text-6xl text-white -rotate-45 relative bottom-5 left-8">→</p> */}
+                        </div>
+                        <p className="text-white text-4xl">Countries</p>
+                    </div>
+                </div>
+               
+            </div>
+
+
+
+
+        </div>
+      </div>
+    </>
+    );
+};
+
+export default Section3;
+
+{/* <div data-aos="fade-left" className="border-t-2 border-l-2 rounded-tl-[100px] pt-32 pl-2 relative bottom-[100px] right-[2px] border-black h-[600px] w-[700px]">
                 <p className="text-7xl mb-0 text-orange-700">PLANNING</p>
                 <p className="text-7xl mb-0 text-orange-600">DESIGNING</p>
                 <p className="text-7xl mb-0 text-orange-500">DECORATION</p>
@@ -24,17 +83,4 @@ const Section3 = () => {
                 <p className="text-7xl mb-0 text-orange-200">MEDIA</p>
                 <img data-aos="zoom-in"  className="h-[350px] relative left-[500px] bottom-[100px]" src="https://i.ibb.co/mR6PFGG/spiral.png" alt="" />
                 
-            </div>
-        </div>
-        <div >
-            <img  className="w-[400px] relative -rotate-[35deg] left-[200px] bottom-[650px] object-cover" src="https://i.ibb.co/2yjw6vG/Online-Signatures-net-830-Touch-Excellence.png" alt="" />
-        </div>
-        <div className="h-[600px]">
-
-        </div>
-      
-    </>
-    );
-};
-
-export default Section3;
+            </div> */}
