d21a7fd54838edf085d1544306a9e552046d621c
hrSajib <harun15-4293@diu.edu.bd>
2024-09-06 17:18:45 +0600

    section2 removed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -44,8 +44,8 @@ const Home = () => {
                     {/* Event type Seminar */}
                     <div id="event1Container" className='relative rounded-2xl w-[45%] h-[38vh] group overflow-hidden'>
                         <img className='h-full w-full rounded-2xl object-cover object-right transition-transform duration-500 ease-in-out group-hover:scale-110' src="https://i.ibb.co/bWHRfhc/1000-F-670307649-4br-Foy-XTCw9-Uc-P3o-NDJRjbqu48-XTHe4-L.jpg" alt="" />
-                        <div id="boxSeminar" className='w-[450px] h-[34vh] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-r-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
-                        <div id="labelSeminar" className='text-orange-500 border-t-[5px] border-orange-400 w-[500px] bg-white text-2xl absolute bottom-8 left-2 transition-all duration-300 ease-in-out group-hover:left-20'>
+                        <div id="boxSeminar" className='w-[46vh] h-[35vh] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-r-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
+                        <div id="labelSeminar" className='text-orange-500 border-t-[5px] border-orange-400 w-[500px] bg-white text-2xl absolute bottom-[5vh] left-2 transition-all duration-300 ease-in-out group-hover:left-20'>
                             <button className='font-josefin-sans relative left-2 transition-all duration-300 ease-in-out group-hover:left-10'>SEMINARS</button>
                         </div>
                     </div>
@@ -60,8 +60,8 @@ const Home = () => {
                             allowFullScreen
                             title="YouTube Video"
                         ></iframe>
-                        <div id="boxSeminar" className='w-[570px] h-[330px] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-r-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
-                        <div id="labelSeminar" className='text-orange-500 border-b-[5px] border-orange-400 w-[680px] h-10 bg-white absolute bottom-8 left-2 transition-all duration-300 ease-in-out group-hover:left-20'>
+                        <div id="boxSeminar" className='w-[57vh] h-[35vh] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-r-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
+                        <div id="labelSeminar" className='text-orange-500 border-b-[5px] border-orange-400 w-[680px] h-10 bg-white absolute bottom-[5vh] left-2 transition-all duration-300 ease-in-out group-hover:left-20'>
                             <button className='font-josefin-sans text-4xl relative left-2 transition-all duration-300 ease-in-out group-hover:left-10'>Parties & Concerts</button>
                         </div>
                     </div>
@@ -74,7 +74,7 @@ const Home = () => {
                         {/* Event type Wedding */}
                     <div id="event2Container" className='mt-3 relative rounded-2xl  w-[55%] h-[38vh] group overflow-hidden'>
                         <img className='w-full h-full rounded-2xl object-cover object-right transition-transform duration-500 ease-in-out group-hover:scale-110' src="https://i.ibb.co/XFG11WC/wedding.jpg" alt="" />
-                        <div id="boxSeminar" className='w-[550px] h-[350px] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-l-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
+                        <div id="boxSeminar" className='w-[57vh] h-[35vh] border-2 border-white transition-opacity duration-500 ease-in-out opacity-40 group-hover:opacity-100 rounded-2xl border-l-4 border-t-4 absolute bottom-[15px] left-6 group-hover:flex '></div>
                         <div id="labelSeminar" className='text-orange-500 h-10 border-b-[5px] border-orange-400 w-[600px] bg-white  text-2xl absolute bottom-10 left-2 transition-all duration-300 ease-in-out group-hover:left-20'>
                             <button className='qwitcher-grypen-bold text-[90px] relative bottom-1 left-2 transition-all duration-300 ease-in-out group-hover:left-10'>Weddings</button>
                         </div>
--- src/components/home/Section2.jsx
+++ src/components/home/Section2.jsx
@@ -10,12 +10,8 @@ const Section2 = () => {
 
 
     return (
-        <div data-aos="fade-left" className="my-56 bg-orange-100 flex py-10 items-center">
-            <img  className="w-[330px] border-4 border-blue-300 z-10 relative left-16 rounded-3xl" src="https://i.ibb.co/QH9CPwX/Exhibitions.jpg" alt="" />
-            <div className="bg-white p-36 h-[300px] rounded-l-[50px] flex flex-col justify-center">
-                    <p className="font-josefin-sans text-5xl text-left mb-5">Explore the exotic</p>
-                    <p data-aos="zoom-in"  className="font-josefin-sans text-xl">We are delighted to have you here! Our passion is bringing your vision to life, whether it’s a wedding, birthday celebration, corporate event, or any special occasion. With our expert planning, stunning decorations, delicious food, and seamless logistics, we ensure every moment is perfect. Let us handle the details so you can enjoy your event without a worry. Your memorable experience starts here!</p>
-            </div>
+        <div className="h-[30vh]">
+           
         </div>
     );
 };
