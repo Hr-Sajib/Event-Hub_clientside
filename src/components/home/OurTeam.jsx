b48ca0bf12dcc26b56e4803aa7bb0e1872c0aef2
hrSajib <harun15-4293@diu.edu.bd>
2024-09-07 14:29:49 +0600

    animation fixed

--- src/components/home/OurTeam.jsx
+++ src/components/home/OurTeam.jsx
@@ -19,7 +19,7 @@ const OurTeam = () => {
             
             
             <div className="bg-orange-950 h-[15vh]">
-                <div className="bg-white h-[18vh] w-[18vh]  relative rounded-full left-[5vw] top-[3vw] text-9xl text-black flex justify-center items-center rotate-45">→</div>
+                <div className="rotateAr shadow-md shadow-orange-900 border border-orange-900 rotate-45 bg-white h-[18vh] w-[18vh]  relative rounded-full left-[5vw] top-[3vw] text-9xl text-black flex justify-center items-center">→</div>
                 
                 <div className="relative bottom-[6.5vh]">
                     <div className="flex justify-center gap-[2vw] ">
@@ -32,7 +32,7 @@ const OurTeam = () => {
             </div>
             
             
-            <div className="flex justify-between h-[60vh] bg-gradient-to-r to-orange-100 from-gray-100 border">
+            <div className="flex justify-between h-[60vh] bg-gradient-to-r to-orange-100 from-gray-100 pr-[5vh]">
                 {/* text  */}
                 <div data-aos="fade-down" className="pt-[10vh] pl-[5vh]">
                     <p className="font-oswald text-9xl">Unite<br/>For<br/>Excellence</p>
@@ -45,8 +45,8 @@ const OurTeam = () => {
                     team.map(person => 
                     
                     <div className="border bg-black rounded-xl w-[50vh] h-[19vh] relative p-2 flex justify-center items-center " key={person.name}>
-                        
-                        <img  data-aos="zoom-in" className="h-[22vh] w-[18vh] pl-2 object-cover" src={person.image} alt="" />
+                          
+                        <img data-aos="zoom-in" className="h-[22vh] w-[18vh] ml-[1vh] object-cover rounded-xl" src={person.image} alt="" />
                         <div className="font-oswald pl-5">
                             <p className="text-white text-3xl">{person.name}</p>
                             <p className="text-orange-300 text-xl">{person.designation}</p>
